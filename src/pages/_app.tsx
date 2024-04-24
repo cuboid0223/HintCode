5a83a8512250759ffc68eb11f3f3f256e7b0c2e2
cuboid0223 <yyytaiwanzed@gmail.com>
2024-04-24 13:02:13 +0800

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>HintCode</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="提供 GPT 產生的個人化提示，提升程式能力"
+        />
+      </Head>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
