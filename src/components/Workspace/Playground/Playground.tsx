11e35a3909519c6435dc550509b32ee3210aa80c
cuboid0223 <yyytaiwanzed@gmail.com>
2024-04-30 20:02:54 +0800

    UI: double tab

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -47,11 +47,12 @@ const Playground: React.FC<PlaygroundProps> = ({
   setSolved,
 }) => {
   const { id } = problem;
-  const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
+
   let [userCode, setUserCode] = useState<string>(problem.starterCode.js);
 
   const [fontSize, setFontSize] = useLocalStorage("lcc-fontSize", "16px");
   const [submissionData, setSubmissionData] = useState<SubmissionData>();
+  const isAccepted = submissionData?.status.id === 3;
   const [settings, setSettings] = useState<Settings>({
     fontSize: fontSize,
     settingsModalIsOpen: false,
@@ -219,67 +220,37 @@ const Playground: React.FC<PlaygroundProps> = ({
 
         <div className="w-full px-5 overflow-auto">
           {/* Tabs: testcase */}
-          <Tabs defaultValue="testcase" className="w-[400px]">
-            <TabsList>
+          <Tabs defaultValue="testcase" className="">
+            <TabsList className="grid w-full grid-cols-2 ">
               <TabsTrigger value="testcase" className="relative">
                 <div className="text-sm font-medium ">測試資料</div>
-                <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
+                <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none " />
               </TabsTrigger>
               <TabsTrigger value="testResult" className="relative">
-                <div className="text-sm font-medium leading-5 text-white">
-                  測試結果
-                </div>
-                <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
+                <div className="text-sm font-medium leading-5 ">測試結果</div>
+                <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none " />
               </TabsTrigger>
             </TabsList>
             <TabsContent value="testcase">
-              <div className="flex">
-                {problem.examples.map((example, index) => (
-                  <div
-                    className="mr-2 items-start mt-2 "
-                    key={example.id}
-                    onClick={() => setActiveTestCaseId(index)}
-                  >
-                    <div className="flex flex-wrap items-center gap-y-4">
-                      <div
-                        className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
-										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
-									`}
-                      >
-                        測資 {index + 1}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-
-              <div className="font-semibold my-4">
-                <p className="text-sm font-medium mt-4 text-white">Input:</p>
-                <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-                  {problem.examples[activeTestCaseId].inputText}
-                </div>
-                <p className="text-sm font-medium mt-4 text-white">Output:</p>
-                <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-                  {problem.examples[activeTestCaseId].outputText}
-                </div>
-              </div>
+              <TestCaseList problem={problem} />
             </TabsContent>
             <TabsContent value="testResult">
               {submissionData ? (
-                <div className="flex items-center ">
-                  <h2
-                    className={`font-bold  text-xl ${
-                      submissionData.status.id === 3
-                        ? "text-green-600"
-                        : "text-red-600"
-                    } `}
-                  >
-                    {submissionData.status.description}
-                  </h2>
-                  <pre className="text-gray-400 text-xs ml-3 ">
-                    Runtime: {submissionData.time} ms
-                  </pre>
-                </div>
+                <>
+                  <div className="flex items-center ">
+                    <h2
+                      className={`font-bold  text-xl ${
+                        isAccepted ? "text-green-600" : "text-red-600"
+                      } `}
+                    >
+                      {submissionData.status.description}
+                    </h2>
+                    <pre className="text-gray-400 text-xs ml-3 ">
+                      Runtime: {submissionData.time} ms
+                    </pre>
+                  </div>
+                  <TestCaseList problem={problem} />
+                </>
               ) : (
                 <h2>沒有測試結果</h2>
               )}
@@ -291,4 +262,48 @@ const Playground: React.FC<PlaygroundProps> = ({
     </div>
   );
 };
+
+// --------------------------------記得移到新的 file--------------------------------
+type TestCaseListProps = {
+  problem: Problem;
+};
+
+const TestCaseList: React.FC<TestCaseListProps> = ({ problem }) => {
+  const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
+  return (
+    <>
+      <div className="flex">
+        {problem.examples.map((example, index) => (
+          <div
+            className="mr-2 items-start mt-2 "
+            key={example.id}
+            onClick={() => setActiveTestCaseId(index)}
+          >
+            <div className="flex flex-wrap items-center gap-y-4">
+              <div
+                className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+									`}
+              >
+                測資 {index + 1}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="font-semibold my-4">
+        <p className="text-sm font-medium mt-4 text-white">Input:</p>
+        <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+          {problem.examples[activeTestCaseId].inputText}
+        </div>
+        <p className="text-sm font-medium mt-4 text-white">Output:</p>
+        <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+          {problem.examples[activeTestCaseId].outputText}
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Playground;
