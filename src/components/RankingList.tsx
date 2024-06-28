e461db051d0e28836118442173f1c55fb0b20926
cuboid0223 <yyytaiwanzed@gmail.com>
2024-06-28 11:36:34 +0800

    refac: fillWithMockUsers should be  pure func

--- src/components/RankingList.tsx
+++ src/components/RankingList.tsx
@@ -47,9 +47,9 @@ function RankingList() {
   const fillWithMockUsers = (users, minNumber: number) => {
     if (users.length < minNumber) {
       const additionalUsers = mockUsersData.slice(0, 10 - users.length);
-      setTop10UsersData([...users, ...additionalUsers]);
+      return [...users, ...additionalUsers];
     } else {
-      setTop10UsersData(users);
+      return users;
     }
   };
 
@@ -94,7 +94,7 @@ function RankingList() {
         });
 
         // 如果獲取到的用戶數少於 10，使用 mock data 補足
-        fillWithMockUsers(usersList, 10);
+        setTop10UsersData(fillWithMockUsers(usersList, 10));
       });
     };
     getTop10UsersData();
