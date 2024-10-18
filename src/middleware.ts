ecb0b060c9ef5388b46868dab158e213d75cd80c
cuboid0223 <yyytaiwanzed@gmail.com>
2024-10-18 12:09:41 +0800

    fix: sentry /monitoring 路由不會與 Next.js 中的 middleware 發生衝突

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,12 @@ import { clientConfig, serverConfig } from "@/config";
 const PUBLIC_PATHS = ["/register", "/login", "/forget-password"];
 
 export async function middleware(request: NextRequest) {
+  // 瀏覽器發送到 Sentry 的請求通過 Next.js 的「重寫（rewrite）」功能路由到 /monitoring，來繞過一些會攔截 Sentry 請求的廣告攔截器（ad-blockers）。
+  // 確保 /monitoring 路由不會與 Next.js 中的 middleware 發生衝突
+  // 或是關掉 adblock
+  if (request.nextUrl.pathname.startsWith("/monitoring")) {
+    return NextResponse.next();
+  }
   return authMiddleware(request, {
     loginPath: "/api/login", // 這個 /api/login 並非自己建立的 api route ，是 next-firebase-auth-edge 設置的常數
     logoutPath: "/api/logout", // 這個 /api/logout 並非自己建立的 api route ，是 next-firebase-auth-edge 設置的常數
